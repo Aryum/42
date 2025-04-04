c5ab3c45bcd35499852afe8ac1edd29e575b3ec6
Ricardo Santos <ricsanto@c1r9s2.42lisboa.com>
2025-04-04 16:29:32 +0100

    w

--- PiscineReloaded/ex14/ft_sqrt.c
+++ PiscineReloaded/ex14/ft_sqrt.c
@@ -6,27 +6,26 @@
 /*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 14:19:15 by ricsanto          #+#    #+#             */
-/*   Updated: 2025/04/04 14:55:21 by ricsanto         ###   ########.fr       */
+/*   Updated: 2025/04/04 16:11:53 by ricsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int testNumber(int nb,int val)
-{
-	int returnVal;
 
-	returnVal = (nb + val) / 2;
-
-	if(returnVal < nb/2)
-	{
-		testNumber(nb, )
-	}
-}
 int ft_sqrt(int nb)
 {
 	int returnVal;
-
-	returnVal = 0;
+	int test;
 	
+	returnVal = 0;
+	test = (returnVal + nb) / 2;
+	if(nb < 0)
+		return 0;
+	while( test < returnVal)
+	{
+		if(returnVal * returnVal == nb)
+			return returnVal;
+		returnVal ++;
+	}
 
 	return 0;
 }
