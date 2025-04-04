7a7905fa3e7be6cf6dfdae1128fb30fe539dcca2
Ricardo Santos <ricsanto@c1r9s2.42lisboa.com>
2025-04-04 14:59:06 +0100

    w

---
+++ PiscineReloaded/ex14/ft_sqrt.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 14:19:15 by ricsanto          #+#    #+#             */
+/*   Updated: 2025/04/04 14:55:21 by ricsanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int testNumber(int nb,int val)
+{
+	int returnVal;
+
+	returnVal = (nb + val) / 2;
+
+	if(returnVal < nb/2)
+	{
+		testNumber(nb, )
+	}
+}
+int ft_sqrt(int nb)
+{
+	int returnVal;
+
+	returnVal = 0;
+	
+
+	return 0;
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("%d", ft_sqrt(2147483647));
+}
