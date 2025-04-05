a5064302377b49eeb2f2eb44dc7561956ddc9c1c
Aryum <117469699+Aryum@users.noreply.github.com>
2025-04-05 14:36:09 +0100

    w

--- PiscineReloaded/ex19/ft_putchar.c
+++ PiscineReloaded/ex19/ft_putchar.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/04 17:14:52 by ricsanto          #+#    #+#             */
-/*   Updated: 2025/04/04 17:38:36 by ricsanto         ###   ########.fr       */
+/*   Created: 2025/04/04 11:36:51 by ricsanto          #+#    #+#             */
+/*   Updated: 2025/04/04 11:47:56 by ricsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putchar(char c);
+#include <unistd.h>
 
-int main(int agrc, char** argv)
+void	ft_putchar(char c)
 {
-	int i;
-	int h;
-	
-	i = 1;
-	h = 0;
-	while(i < agrc)
-	{
-		while (argv[i][h] != '\0')
-		{
-			ft_putchar(argv[i][h]);
-			h++;
-		}
-		ft_putchar('\n');
-		h = 0;
-		i++;
-	}
-	return 0;
+	write(1, &c, 1);	
 }
---
+++ PiscineReloaded/ex19/ft_sort_params.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 13:54:27 by ricsanto          #+#    #+#             */
+/*   Updated: 2025/04/05 14:35:59 by ricsanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+void ft_putchar(char c);
+
+int strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while((s1[i] != '\0' || s2[i] != '\0') && s1[i] == s2[i])
+		i++;
+	return((unsigned char) s1[i] - (unsigned char) s2[i]);
+}
+
+void swap(char **a, char **b)
+{
+	char **temp;
+	
+	**temp = **a;
+	**a = **b;
+	**b = **temp;
+}
+
+void sort(char** arr, int length)
+{
+	int i;
+	int h;
+	int swapped;
+	
+	i = 0;
+	h = 1;
+	swapped = 0;
+	while (i < length)
+	{
+		//do a bubble sort
+	}
+}
+
+int main(int agrc, char** agrv)
+{
+	int i;
+	int h;
+
+	h = 0;
+	i = 1;
+	sort(agrv,agrc);
+	while (i < agrc)
+	{
+		while(agrv[i][h] != '\0')
+		{
+			ft_putchar(agrv[i][h]);
+			h++;
+		}
+		ft_putchar('\n');
+
+		h = 0;
+		i++;
+	}
+}
