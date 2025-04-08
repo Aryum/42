c836eec7a76ce5397c7cb97e071a7d8878244b67
Aryum <ricardo.parcelas.santos@gmail.com>
2025-04-08 10:45:37 +0100

    w

---
+++ PiscineReloaded/ex27/ft_display_file.c
@@ -0,0 +1,12 @@
+//errors
+//when no argument is given
+//	File name missing.
+//when more than one argument is given
+//	Too many arguments.
+//if the file cannot be read
+//	Cannot read file.
+
+int main(int argc, char *argv)
+{
+	
+}
