6c84354d7d695d8d94b9decf263f383c5f10e10f
Ricardo Santos <ricsanto@c1r8s5.42lisboa.com>
2025-04-08 17:27:19 +0100

    w

--- PiscineReloaded/ex13/ft_recursive_factorial.c
+++ PiscineReloaded/ex13/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 12:20:54 by ricsanto          #+#    #+#             */
-/*   Updated: 2025/04/08 17:11:11 by ricsanto         ###   ########.fr       */
+/*   Updated: 2025/04/08 17:17:52 by ricsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0 || (long)((long)nb * ((long)(nb - 1)) ) > 2147483648)
+	if (nb < 0 || nb > 13)
 		return (0);
 	else if (nb == 0)
 		return (1);
@@ -23,5 +23,5 @@ int	ft_recursive_factorial(int nb)
 #include <time.h>
 int main()
 {
-	printf("%d", ft_recursive_factorial(18));	
+	printf("%d", ft_recursive_factorial(13));	
 }
--- PiscineReloaded/readme.txt
+++ PiscineReloaded/readme.txt
@@ -1,4 +1,4 @@
-ex13: KO |
+ex13: KO | corrected
 ex14: KO | 
 ex19: Does not compile | 
 ex27: Norme error
