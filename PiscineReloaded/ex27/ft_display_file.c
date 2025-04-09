31bab5098ffa4947e5dd389cc3372afe14b0621c
Ricardo Santos <ricsanto@c1r9s2.42lisboa.com>
2025-04-09 13:04:50 +0100

    w

--- PiscineReloaded/ex27/ft_display_file.c
+++ PiscineReloaded/ex27/ft_display_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_display_file.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ricardo-santos <ricardo-santos@student.    +#+  +:+       +#+        */
+/*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 09:47:32 by ricardo-san       #+#    #+#             */
-/*   Updated: 2025/04/09 09:48:54 by ricardo-san      ###   ########.fr       */
+/*   Updated: 2025/04/09 13:02:42 by ricsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	putstr(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		write(1, &str[i], 1);
+		write(2, &str[i], 1);
 		i++;
 	}
 }
@@ -45,15 +45,15 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 	{
 		if (argc == 1)
-			putstr("File name missing.");
+			putstr("File name missing.\n");
 		else
-			putstr("Too many arguments.");
+			putstr("Too many arguments.\n");
 		return (0);
 	}
 	file = open(argv[1], O_RDONLY);
 	if (file == -1)
 	{
-		putstr("Cannot read file.");
+		putstr("Cannot read file.\n");
 		return (0);
 	}
 	displayfile(file);
