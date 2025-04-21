a9409e353ed661ce9356383c6d429e42b0e815a2
Aryum <ricardo.parcelas.santos@gmail.com>
2025-04-21 09:49:25 +0100

    w

--- CommonCore/Libft/Exercises/needs tests/ft_lstadd_front.c
+++ CommonCore/Libft/Exercises/needs tests/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 09:30:39 by ricsanto          #+#    #+#             */
-/*   Updated: 2025/04/21 09:31:45 by ricsanto         ###   ########.fr       */
+/*   Updated: 2025/04/21 09:49:21 by ricsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ void ft_lstadd_front(t_list **lst, t_list *new)
 	t_list	temp;
 
 	temp = (**lst);
-		
+	(*new).next = (*lst);
+	*lst = new; 		
 }
