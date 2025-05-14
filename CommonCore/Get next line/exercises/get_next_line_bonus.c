19203599646aa360f416c415ae9cb73e2c6d9f91
Aryum <ricardo.parcelas.santos@gmail.com>
2025-05-14 10:19:56 +0100

    w

--- CommonCore/Get next line/exercises/get_next_line_bonus.c
+++ CommonCore/Get next line/exercises/get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ricsanto <ricsanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:55:55 by ricsanto          #+#    #+#             */
-/*   Updated: 2025/05/14 09:28:32 by ricsanto         ###   ########.fr       */
+/*   Updated: 2025/05/14 10:19:41 by ricsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*get_next_line(int fd)
 	ssize_t		readbytes;
 	char		*ret;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > FOPEN_MAX || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE < 1|| fd >= FOPEN_MAX || read(fd, 0, 0) < 0)
 		return (NULL);
 	ret = malloc(1);
 	ret[0] = '\0';
